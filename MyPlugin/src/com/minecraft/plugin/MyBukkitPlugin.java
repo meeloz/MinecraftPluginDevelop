049b065902dcea7815d4ea9dc67b2ee58372be78
meeloz <71424874+meeloz@users.noreply.github.com>
2020-10-10 07:02:01 +0800

    20201010

--- MyPlugin/src/com/minecraft/plugin/MyBukkitPlugin.java
+++ MyPlugin/src/com/minecraft/plugin/MyBukkitPlugin.java
@@ -3,15 +3,27 @@ package com.minecraft.plugin;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
+import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class MyBukkitPlugin extends JavaPlugin implements Listener {
+    private final List<String> list = Arrays.asList("ARMORER.yml", "BUTCHER.yml", "FARMER.yml","CARTOGRAPHER.yml","CLERIC.yml","FISHERMAN.yml","FLETCHER.yml","LEATHERWORKER.yml","LIBRARIAN.yml","MASON.yml","TOOLSMITH.yml","WEAPONSMITH.yml","SHEPHERD.yml");
+
 
     public void onEnable() {
         saveDefaultConfig();
-        reloadConfig();
+        fileProfession();
+
         PluginManager manager = getServer().getPluginManager();
         manager.registerEvents(this, this);
         manager.registerEvents(new PlayerEvent(this), this);
@@ -30,5 +42,26 @@ public class MyBukkitPlugin extends JavaPlugin implements Listener {
         }
         return false;
     }
+
+    public void fileProfession(){
+        final File dataFolder = this.getDataFolder();
+        final File files = new File(dataFolder, "Profession");
+        if (!files.exists()) {
+            files.mkdirs();
+        }
+        for(String fileName:list) {
+            final File file = new File(files, fileName);
+            if (!file.exists()) {
+                InputStream inputStream = this.getResource("Profession/" + fileName);
+                try {
+                    Files.copy(inputStream, file.toPath());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+
 
 }
--- MyPlugin/src/com/minecraft/plugin/PlayerEvent.java
+++ MyPlugin/src/com/minecraft/plugin/PlayerEvent.java
@@ -1,34 +1,48 @@
 package com.minecraft.plugin;
 
+import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.entity.*;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.*;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.MerchantRecipe;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Random;
 
 public class PlayerEvent implements Listener{
 
     private final MyBukkitPlugin MyBukkitPlugin;
+    private YamlConfiguration configuration;
+
 
     public PlayerEvent(MyBukkitPlugin MyBukkitPlugin){
         this.MyBukkitPlugin = MyBukkitPlugin;
     }
 
     @EventHandler
-    public void VillagerAcquireTradeEvent(VillagerAcquireTradeEvent e) {
-        FileConfiguration f = MyBukkitPlugin.getConfig();
+    public void VillagerAcquireTradeEvent(VillagerAcquireTradeEvent e) throws IOException {
         Villager v = (Villager)e.getEntity();
+        configuration = YamlConfiguration.loadConfiguration(new File(MyBukkitPlugin.getDataFolder(), "Profession/" + v.getProfession()+".yml"));
+
         String lv = "level_"+v.getVillagerLevel();
         String recipe = rndRecipe(lv);
         ItemStack result = getResult(recipe);
+        int exp = getExp(recipe);
+
         ArrayList<ItemStack> require = getRequire(recipe);
 
-        MerchantRecipe r = new MerchantRecipe(result,0,10,true,50,(float)1.0);
+        MerchantRecipe r = new MerchantRecipe(result,0,10,true,exp,(float)1.0);
         for(int i = 0;i < require.size();i++) {
             r.addIngredient(require.get(i));
         }
@@ -37,7 +51,7 @@ public class PlayerEvent implements Listener{
 
     //隨機取得一個配方
     public String rndRecipe(String lv){
-        FileConfiguration f = MyBukkitPlugin.getConfig();
+        FileConfiguration f = configuration;
         Object[] array = f.getConfigurationSection(lv).getKeys(false).toArray();
         int rnd = new Random().nextInt(array.length);
         String recipe = lv+"."+array[rnd];
@@ -45,7 +59,7 @@ public class PlayerEvent implements Listener{
     }
 
     public ItemStack getResult(String recipe){
-        FileConfiguration f = MyBukkitPlugin.getConfig();
+        FileConfiguration f = configuration;
         String result = f.getString(recipe+"."+".result");
         int cutindex = result.indexOf(",");
         String item = result.substring(0,cutindex);
@@ -54,7 +68,7 @@ public class PlayerEvent implements Listener{
     }
 
     public ArrayList<ItemStack> getRequire(String recipe){
-        FileConfiguration f = MyBukkitPlugin.getConfig();
+        FileConfiguration f = configuration;
         //取得物品List並轉成陣列,List打印後: [IRON_SWORD,1,DIAMOND,1]
         Object[] require = f.getStringList(recipe+"."+".require").toArray();
 
@@ -68,8 +82,9 @@ public class PlayerEvent implements Listener{
         return req;
     }
 
-
-
-
-
+    public int getExp(String recipe){
+        FileConfiguration f = configuration;
+        int exp = f.getInt(recipe+"."+".exp");
+        return exp;
+    }
 }
